b9f2881d98d0c82c12ded478cdc9440e961e14fe
E-paiva <141731995+E-paiva@users.noreply.github.com>
2024-03-14 21:31:48 +0000

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
+import { isPageStatic } from 'next/dist/build/utils';
+import { Modern_Antiqua } from 'next/font/google';
+
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
     images:{
         unoptimized: true,
     },
-    
+    basePath: isProd ? '/colorapp':'',    
+    assetPrefix: isProd ? '/colorapp/' :'',
 };
+
+
 
 export default nextConfig;
